4f342f75d922bca72c534757ba58de93d253e20b
Emily Perez-Rodriguez <emilyp.rodriguez632@gmail.com>
2023-06-08 18:12:03 -0400

    implemented about page and started todo list

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../component/todos.css";
+import {Card,CardContent,Grid,ListItemButton,ListItemText,Checkbox,} from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -14,7 +15,7 @@ const Todos = ({ todos }) => {
         return (
           <Grid key={todo.id}>
             <Card>
-              {/* Remember, we set the local state of this todo item when the user submits the form in 
+              {todo.content/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
               <CardContent>
                 <span style={{ padding: "50px" }}>add todo content</span>
@@ -24,12 +25,12 @@ const Todos = ({ todos }) => {
         );
       })
     ) : (
-      <p>{/*Enter your message here if todo list is empty*/}</p>
+      <p>You have no todos left</p>
     );
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
-        {/*call todo list here*/}
+        {todoList/*call todo list here*/}
       </div>
     );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-  
+
+import "./About.css";
+import profile_pic_name from "../assets/profile_pic_name.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p>
+          <div>
+            <div className="split left">
+              <div className="centered">
+                <img
+                  className="profile_image" src={profile_pic_name}
+                  // Image goes here
+                  alt="Profile Pic"
+                ></img>
+              </div>
+            </div>
+            <div className="split right">
+              <div className="centered">
+                <div className="name_title">Emily Perez-Rodriguez</div>
+                <div className="brief_description">
+                  I am a computer science and public health studies student from Baltimore, Maryland. I also love OnePiece.
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </p>
       </div>
     )
   }
